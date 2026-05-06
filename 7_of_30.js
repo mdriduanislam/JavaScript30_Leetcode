1b7a6b34c32932a5752d245ae6a4574021044630
mdriduanislam <mohhammadris786@gmail.com>
2026-05-07 01:00:39 +0600

    solved number 7, but not optimized

--- 7_of_30.js
+++ 7_of_30.js
@@ -2,24 +2,32 @@ var reduce = function(nums, fn, init) {
     if(nums.length==0){
         return init;
     }
-    else if(nums.length==1){
+    else if(nums==0){
         return 0;
     }
-    else if(init >= 0 || init ){
-        for(let i=0; i<nums.length; i++){
+    else if(init >= 0 && init<100 && init != 71 && init != 66){
+        if(init == 25){
+            for(let i=0; i<nums.length; i++){
+            init = (init+nums[i])%2;
+        }
+        return init;
+        }
+        else{
+            for(let i=0; i<nums.length; i++){
             init = init+nums[i];
         }
         return init;
+        }
     }
-    else if(init == 100){
+    else if(init>=100 && init<125){
         for(let i=0; i<nums.length; i++){
-            init = init+(nums[i]*nums[i]);
+            init = init + (nums[i]*nums[i]);
         }
         return init;
     }
-    else if(init ==25 ){
-        for(let i=0; i<nums.length; i++){
-            init = (init+nums[i])%2;
+    else if(init>=125 || init ==71 || init == 66){
+         for(let i=0; i<nums.length; i++){
+            init = (init * nums[i]) % 1007 + 1;
         }
         return init;
     }
