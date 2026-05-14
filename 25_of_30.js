e6ca1a131c1931f7b42d4af1807bfef0cf53c4d3
mdriduanislam <mohhammadris786@gmail.com>
2026-05-14 23:50:32 +0600

    solved 25 of 30

---
+++ 25_of_30.js
@@ -0,0 +1,12 @@
+var join = function(arr1, arr2) {
+    let merged = new Map();
+    for (let obj of [...arr1, ...arr2]) {
+       let existing = merged.get(obj.id) || {};
+       merged.set(obj.id, {
+          ...existing,
+          ...obj
+       });
+    }
+  let result = [...merged.values()];
+  return result;
+};
